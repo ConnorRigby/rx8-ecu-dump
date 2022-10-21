c24aea3275b74323b72e2e78032d31c00024e3c3
Connor Rigby <connorr@hey.com>
2022-10-20 23:11:21 -0700

    disassembly: sleeeeeeep
    Signed-off-by: Connor Rigby <connorr@hey.com>

--- disassembly/src/uds_transfer_get_length.c
+++ disassembly/src/uds_transfer_get_length.c
@@ -3,7 +3,7 @@
 typedef uint8_t byte;
 
 
-uint8_t uds_request_download_get_address_offset(byte *payload)
+int uds_request_download_get_chunk_size(byte *payload)
 
 {
   return ((int)(char)payload[1] & 0xffU) * 0x10000 + ((int)(char)payload[2] & 0xffU) * 0x100 +
@@ -17,16 +17,48 @@ int uds_transfer_get_length(byte *payload)
          ((int)(char)payload[2] & 0xffU);
 }
 
+char* RAM_START;
+
 int main(int argc, char const *argv[])
 {
-  // byte payload[3] = {0, 0x07, 0xf8};
-  byte payload[] = {0x34, 00, 00, 0x40, 00, 00, 0x07, 0xF8, 00, 00, 00, 00, 00};
-  int value = uds_transfer_get_length(payload+5);
-  printf("value=0x%08X\n\n", value);
-  printf("value-0x7e000=0x%08X\n\n\n", value-0x7e000);
+  int chunk_size, length;
+  char payload[] = { 0x00, 0x00, 0x40, 0x00, 0x00,  0x07, 0xF8, 0x00, 0x00, 0x00, 0x00, 0x00};
+  chunk_size = uds_request_download_get_chunk_size(payload);
+  printf("chunksize=%08x\n\n", chunk_size);
+              /* firmware gets coppied here */
+  RAM_START = 0xffff6000;
+  int uds_transfer_start_ptr = RAM_START + chunk_size;
+  printf("uds_transfer_start_ptr=%08x\n\n", uds_transfer_start_ptr);
 
-  value = uds_request_download_get_address_offset(payload);
-  printf("value=0x%08X\n\n", value);
+  chunk_size = uds_transfer_get_length(payload + 5);
+  int uds_transfer_bytes_remaining = chunk_size - 0x7e000;
+  int uds_transfer_address = uds_transfer_start_ptr;
+  if ((0 < uds_transfer_bytes_remaining) /* &&
+      ((uds_frame *)(uds_transfer_start_ptr + chunk_size + -0x7e001) < frame)*/) {
+    printf("0 < uds_transfer_bytes_remaining %08x\n", uds_transfer_bytes_remaining);
+    if (uds_transfer_bytes_remaining < 0) {
+      length = ~(~uds_transfer_bytes_remaining + 1U & 0x3ff) + 1;
+      printf("uds_transfer_bytes_remaining < 0 %08x \n", length);
+    }
+    else {
+      length = uds_transfer_bytes_remaining & 0x3ff;
+      printf("uds_transfer_bytes_remaining & 0x3ff %08x \n", length);
+    }
+    if (length == 0) {
+      printf("length = 0\n");
+      // uds_flash_unlocked = false;
+      // uds_download_in_progress = true;
+      // UDS_REPLY.sid = UDS_SID_REQUEST_DOWNLOAD_ACK;
+      // UDS_REPLY.sbf = 4;
+      // UDS_REPLY.data[0] = 1;
+              /* 7E8#03 74 04 01 00 00 00 00 */
+      // UDS_REPLY.length = 2;
+      // uds_download_kernel_complete = false;
+      // uds_transfer_counter = 1;
+      // goto uds_handle_request_download_send_frame;
+    }
+  }
+
 
   /* code */
   return 0;
