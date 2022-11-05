9f64a55fe6b77bcfdb5509db0e9a44b607f632dc
Connor Rigby <connorr@hey.com>
2022-11-05 11:21:46 -0700

    Remove `debug` utility function
    Signed-off-by: Connor Rigby <connorr@hey.com>

--- src/util.h
+++ src/util.h
@@ -51,7 +51,6 @@ void sleep_ms(int milliseconds);
 #define DEBUG
 #ifdef DEBUG
 #define log_location stderr
-#define debug(...) do { fprintf(log_location, __VA_ARGS__); fprintf(log_location, "\r\n"); fflush(log_location); } while(0)
 #define error(...) do { debug(__VA_ARGS__); } while (0)
 #else
 #define debug(...)
